1335a297a2f6d24197610b9142cd4a87f294dc72
Golen <golen87@hotmail.com>
2022-12-06 00:32:36 +0100

    Change routing to hash based to allow single-page-app to work on github pages

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import HomePage from './components/HomePage'
 import StaffPage from './components/StaffPage'
@@ -44,7 +44,7 @@ const routes = [
 
 const router = createRouter({
 	mode: 'history',
-	history: createWebHistory(),
+	history: createWebHashHistory(),
 	routes,
 });
 
