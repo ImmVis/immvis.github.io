ebabc7236edee8765ef5777c015e5f7015c64563
Alexander Bock <mail@alexanderbock.eu>
2022-12-06 01:58:29 +0100

    Add more exjobb works

--- src/exjobb/list.js
+++ src/exjobb/list.js
@@ -2,10 +2,14 @@ import hajisharif_miandji_video_compression from './markdowns/hajisharif_miandji
 import hajisharif_miandji_learning_based_rendering from './markdowns/hajisharif_miandji_learning_based_rendering.js';
 import jonsson_neural_network_visualization from './markdowns/jonsson_neural_network_visualization.js';
 import akbaba_tracing_research_insights from './markdowns/akbaba_tracing_research_insights.js';
+import broman_astrocasting from './markdowns/broman_astrocasting.js';
+import broman_moldome from './markdowns/broman_moldome.js';
 
 export default [
 	hajisharif_miandji_video_compression,
 	hajisharif_miandji_learning_based_rendering,
 	jonsson_neural_network_visualization,
 	akbaba_tracing_research_insights,
+	broman_astrocasting,
+	broman_moldome
 ];
---
+++ src/exjobb/markdowns/broman_moldome.js
@@ -0,0 +1,44 @@
+const markdown = `
+
+Two of the software packages developed at the MIT division are [OpenSpace](https://openspaceproject.com), an open-source astrovisualization software placing our known universe into context, and [VIAMD](https://github.com/scanberg/viamd), which is a powerful analysis tool for dynamic molecular simulations.  The goal of this thesis work is to combine the ability of OpenSpace to render in immersive display environments and the analysis capabilities of VIAMD which are designed for single flat-screen interaction.  The combination of these two software packages will enable the domain scientists to perform their analysis of complex molecular processes in, for example, planetariums, using VR headsets and more.  This project will require familiarity with C++.
+
+##### Useful links
+- [VIAMD manual](https://github.com/scanberg/viamd/wiki)
+- Draft of the previous paper on request
+
+## Application
+<!--
+Please send an email indicating your interest for this or other thesis works to [emma.broman@liu.se](mailto:emma.broman@liu.se) including your CV/LinkedIn and a Ladok transcript of courses until EOD **15th of October, 2022**. If you are already a pair wanting to work on the thesis together or you are interested in multiple thesis works, a single email will suffice. 
+
+Applications are accepted on a rolling basis.
+-->
+
+Applications for this project are still accepted. Please send an email indicating your interest for this thesis work to [emma.broman@liu.se](mailto:emma.broman@liu.se), including your CV/LinkedIn and a Ladok transcript of courses. If you are already a pair wanting to work on the thesis together or you are interested in multiple thesis works, a single email will suffice.
+`;
+
+export default {
+	id: "broman_moldome",
+	title: "Molecular Rendering in OpenSpace",
+	description: "",
+	contact: [
+		{
+			name: "Emma Broman",
+			url: "https://liu.se/en/employee/emmbr26",
+			image: "https://liu.se/-/media/employeeimages/26/employee_image_emmbr26.jpeg"
+		},
+		{
+			name: "Alexander Bock",
+			url: "https://liu.se/en/employee/alebo68",
+			image: "https://liu.se/-/media/images/modules/foundation/contact/profile500x500.png"
+		},
+		{
+			name: "Mathieu Linares",
+			url: "https://liu.se/en/employee/matli20",
+			image: "https://liu.se/-/media/employeeimages/20/employee_image_matli20.jpeg"
+		},
+	],
+	location: "Norrköping",
+	students: 1,
+	image: null,
+	markdown,
+};
